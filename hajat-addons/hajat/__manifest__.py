4da7ce821192950e8f84f2c987d6d31d150c0143
MElkmeshi <elkmeshi2002@gmail.com>
2024-08-20 23:30:20 +0200

    remove _2

--- hajat-addons/hajat/__manifest__.py
+++ hajat-addons/hajat/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Returns products in JSON format, and creates contacts using an API endpoint',
     'description': 'An Odoo module that provides an endpoint to return products in JSON format',
     'author': 'Mohamed Elkmeshi',
-    'depends': ['base', 'product','contacts','product_brand_2','website_sale','sale'],
+    'depends': ['base', 'product','contacts','product_brand','website_sale','sale'],
     'data': [
         'views/product_template_views.xml',
     ],
